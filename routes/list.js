65911342f3070f0378c74b7c5cd543e00aa31e19
Adiya <adiya.sakhipkereyeva@gmail.com>
2022-04-20 07:05:59 +0600

    routes

---
+++ routes/list.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const ejs = require("ejs");
+router
+    .route("/")
+    .get((req, res) => res.render(path.resolve("./views/list.ejs")))
+    .post((req, res) => res.send("POST"));
+module.exports = router;
